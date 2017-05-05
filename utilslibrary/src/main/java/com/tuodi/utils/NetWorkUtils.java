5a3428fbaadfb934c73bbf4d979e72ecf9f2a633
dsliang <zq2114522@163.com>
2017-05-05 15:44:51 +0800

    添加NetworkUtils,用于检测网络是否可用

---
+++ utilslibrary/src/main/java/com/tuodi/utils/NetWorkUtils.java
@@ -0,0 +1,32 @@
+package com.tuodi.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by dsliang on 2017/5/5.
+ */
+
+public class NetWorkUtils {
+
+    /**
+     * 检测当的网络（WLAN、3G/2G）状态
+     *
+     * @param context Context
+     * @return true 表示网络可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected()) {
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
